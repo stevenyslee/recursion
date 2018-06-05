d4a562a0ece8f051390038131a5597f12bc5d0aa
Steven Lee <Steven@blueberry.lan>
2018-06-04 21:02:34 -0700

    Implement stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// var getElementsByClassName = function(className) {
+// 	let array = [];
+//   if( this.childNodes ){
+//   	for( let i = 0; i < this.childNodes; i++ ){
+//   		let childNode = this.childNodes[i];
+//   		if( this.childNodes[i].className === className ){
+//   			array.push(this.childNodes[i]);
+//   		}
+//   		this.childNodes[i].getElementsByClassName(className);
+//   	}
+//   } else {
+// 		if( this.className === className ){
+// 				array.push(this);
+// 		}
+// 		return;
+//   }
+//   return;
+// };
+
+var getElementsByClassName = function(className) {
+	let array = [];
+	if( this.className === className ){
+		array.push(this);
+	}
+  if( this.childNodes ){
+  	for( let i = 0; i < this.childNodes; i++ ){
+  		this.childNodes[i].getElementsByClassName(className);
+  	}
+  }
+  return;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if( typeof obj === 'undefined' || obj === 'undefined' ){
+		return '';
+	}
+	if( obj === null ){
+		return 'null';
+	}
+	if( typeof(obj) === 'function' ){
+		return '';
+	}
+	if( typeof(obj) === 'object' && !Array.isArray(obj) ){
+		let string = '{';
+		let keys = Object.keys(obj);
+		for( let i = 0; i < keys.length; i++ ){
+			if( keys[i] !== 'functions' && keys[i] !== 'undefined' ){
+				string += '"' + keys[i] + '":' + stringifyJSON( obj[keys[i]] );
+				if( i !== keys.length - 1 ){
+					string += ',';
+				}
+			}
+		}
+		return string += '}';
+	}
+	if( Array.isArray(obj) ){
+		let string = '[';
+		for( let i = 0; i < obj.length; i++ ){
+			string += stringifyJSON(obj[i]);
+			if( i !== obj.length - 1){
+				string += ',';
+			}
+		}
+		return string + ']';
+	}
+	if( typeof(obj) === 'string' ){
+		return '"' + obj + '"';
+	}
+	else {
+		return '' + obj;
+	}
 };
